abc025cccb3670be15273ae3e63e2c261547599b
ibrahim <ibrahim.binburikan@gmail.com>
2025-08-15 06:50:40 +0300

    Update 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,26 +1,10 @@
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.getElementById('menu');
+    const navbar = document.querySelector('.navbar');
 
-    $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
+    menu.addEventListener('click', function () {
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('nav-toggle');
     });
 });
 
-// disable developer mode
-document.onkeydown = function(e) {
-  if(e.keyCode == 123) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-     return false;
-  }
-  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-     return false;
-  }
-}
